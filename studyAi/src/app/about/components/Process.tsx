f2fe21d331d451cec6ab9abfc9b0079a000b8ed6
Connor_Crump <ccrump@uoregon.edu>
2023-10-13 13:23:38 -0700

    Add style to Process component

--- studyAi/src/app/about/components/Process.tsx
+++ studyAi/src/app/about/components/Process.tsx
@@ -1,29 +1,29 @@
 "use client"
 export default function Process(){
     return(
-        <div>
-            <h4><center>Why use Ai?</center></h4>
-            <h1><center>Understanding the Process</center></h1>
-            <h4><center>Dive how generating novel practice questions, create better exam results!</center></h4>
-            <div className="flex justify-between">
-                <div className="w-1/3 bg-gray-200 p-4 m-2 text-center">
-                <img></img>
-                    <h1>Why it works?</h1>
+        <div className="py-12 px-2">
+            <h4 className="text-sm font-bold"><center>Why use Ai?</center></h4>
+            <h1 className="text-6xl font-bold my-3"><center>Understanding the Process</center></h1>
+            <h4><center>Dive into generating novel practice questions and creating better exam results!</center></h4>
+            <div className="sm:flex justify-between mt-8">
+                <div className="sm:w-1/3 bg-gray-200 p-4 m-2 text-center border-2 h-48">
+                    <img></img>
+                    <h1 className="text-2xl font-bold">How It Works</h1>
                     <p>Discover how novel questions are created!</p>
-                    <h4><img></img>Learn more</h4>
+                    <button><img></img>Learn more</button>
                 </div>
-                <div className="w-1/3 bg-gray-200 p-4 m-2 text-center">
-                <img></img>
-                    <h1>Why it works?</h1>
+                <div className="sm:w-1/3 bg-gray-200 p-4 m-2 text-center border-2 h-48">
+                    <img></img>
+                    <h1 className="text-2xl font-bold">Why It Works</h1>
                     <p>Discover the power of Ai technology</p>
-                    <h4><img></img>Learn more</h4>
+                    <button><img></img>Learn more</button>
 
                 </div>
-                <div className="w-1/3 bg-gray-200 p-4 m-2 text-center">
+                <div className="sm:w-1/3 bg-gray-200 p-4 m-2 text-center border-2 h-48">
                     <img></img>
-                    <h1>Why it works?</h1>
-                    <p>Contribute the power of our endless technology</p>
-                    <h4><img></img>Learn more</h4>
+                    <h1 className="text-2xl font-bold">Join us</h1>
+                    <p>Contribute and use our endlessly growing technology</p>
+                    <button><img></img>Sign Up</button>
                 </div>
             </div>
         </div>
