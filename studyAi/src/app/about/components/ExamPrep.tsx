6b82051902a4c0d33927492089b2d1835ec1fdf5
Connor_Crump <ccrump@uoregon.edu>
2023-10-17 12:44:16 -0700

    Update styles in ExamPrep component of about us page

--- studyAi/src/app/about/components/ExamPrep.tsx
+++ studyAi/src/app/about/components/ExamPrep.tsx
@@ -1,12 +1,12 @@
 export default function ExamPrep(){
     return(
-        <div className="sm:flex justify-between items-center py-28 px-16 text-center sm:text-left">
+        <div className="sm:flex justify-between items-center py-16 px-5 sm:py-28 sm:px-16 text-center sm:text-left">
             <div> 
                 <h1 className="text-5xl sm:text-7xl font-bold">Experience our innovative exam prep</h1>
                 <h2 className="sm:text-xl mt-4">Unlock your full potential with our AI-powered practice questions</h2>
             </div>
             <div className="flex mt-4">
-                <div>
+                <div className="mx-auto">
                     <button className="bg-Black border text-White py-3 px-6 mr-4">Try</button>
                     <button className="border py-3 px-6">learn More</button>
                 </div>
