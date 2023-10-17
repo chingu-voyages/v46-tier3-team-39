4694f2ec9469f1fc82e6ebca6b001b8fdb521582
Connor_Crump <ccrump@uoregon.edu>
2023-10-17 10:28:39 -0700

    Setup styles for ExamPrep section of about us page

--- studyAi/src/app/about/components/ExamPrep.tsx
+++ studyAi/src/app/about/components/ExamPrep.tsx
@@ -1,14 +1,14 @@
 export default function ExamPrep(){
     return(
-        <div>
-            <div className="column"> 
-                <h1>Experience our innovative exam prep</h1>
-                <h4>Unlock your full potential with our AI-powered practice questions</h4>
+        <div className="sm:flex justify-between items-center py-28 px-16 text-center sm:text-left">
+            <div> 
+                <h1 className="text-5xl sm:text-7xl font-bold">Experience our innovative exam prep</h1>
+                <h2 className="sm:text-xl mt-4">Unlock your full potential with our AI-powered practice questions</h2>
             </div>
-            <div className="column"> 
-                <div className="align">
-                    <button className="bg-black text-white px-4 py-2">Try</button>
-                    <button className="bg-white text-black px-4 py-2">learn More</button>
+            <div className="flex mt-4">
+                <div>
+                    <button className="bg-Black border text-White py-3 px-6 mr-4">Try</button>
+                    <button className="border py-3 px-6">learn More</button>
                 </div>
             </div>
         </div>
