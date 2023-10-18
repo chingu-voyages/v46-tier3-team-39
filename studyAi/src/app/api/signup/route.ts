652bbaa53c94f28a032c92cb500fe8be10fa4af1
TiffChan <tiffanychan1999614@gmail.com>
2023-10-17 20:17:46 -0400

    Update POST api for users

--- studyAi/src/app/api/signup/route.ts
+++ studyAi/src/app/api/signup/route.ts
@@ -8,19 +8,23 @@ export async function POST(req: NextRequest, res: NextResponse) {
     const body = await req.json();
     const { name, email, password } = body;
 
+    console.log('name, email, password: ' + name, email, password)
+
     if (!name || !email || !password) {
         return new NextResponse("Missing Fields", { status: 400 });
       }
 
-      const foundUser = await ExecuteQuery(`SELECT name, email, password FROM dbo.users
-            WHERE email = ${email}`);
+      const foundUser = null;
+    //   await ExecuteQuery(`SELECT name, email, password FROM dbo.users
+    //         WHERE email = ${email}`);
       if (foundUser) {
 
         return new NextResponse("User already in database", { status: 404 });
       }
 
       const hashedPassword = await bcrypt.hash(password, 10);
-      await ExecuteQuery(`INSERT INTO dbo.users (name, email, password) VALUES (${name}, ${email}, ${hashedPassword})`);
+
+      await ExecuteQuery(`INSERT INTO dbo.users (name) VALUES ('${name}');`);
 
 //   console.log(await ExecuteQuery("select * from dbo.users"));
 
