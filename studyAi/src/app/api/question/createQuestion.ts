331c8cbeff0760b18f1513832202e4b3b78d7a4a
Irha Ali <irhaali_97@hotmail.ca>
2023-10-22 10:26:17 -0400

    Question Routes

---
+++ studyAi/src/app/api/question/createQuestion.ts
@@ -0,0 +1,60 @@
+import { prismaDb, connectToDb } from "@/app/util/prisma/helpers";
+import { NextResponse } from "next/server";
+import * as z from "zod";
+
+//schema for validating user inputs
+const questionSchema = z.object({
+  creatorId: z.string(),
+  type: z.string(),
+  tags: z.array(z.string()),
+  question: z.object({
+    title: z.string(),
+    description: z.string()
+  }),
+  answer: z.object({
+    answer: z.string(),
+  }),
+  likeCounter: z.object({
+    likes: z.number(),
+    dislikes: z.number()
+  })
+});
+
+export async function createQuestion(req: Request) {
+  try {
+    const bodyPromise = req.json();
+    const [body, _] = await Promise.all([bodyPromise, connectToDb()]);
+    const { creatorId, type, tags, question, answer, likeCounter } = questionSchema.parse(body);
+    console.log("28: HERERERERERERERERERERERERERERERE");
+
+    console.log(creatorId, type, tags, question, answer, likeCounter)
+
+    const newQuestionPromise = prismaDb.question.create({
+      data: {
+        creatorId,
+        type,
+        tags,
+        question,
+        answer,
+        likeCounter
+      },
+    });
+    
+    const newQuestion = await Promise.all([
+      newQuestionPromise
+    ]);
+
+    return NextResponse.json({
+      newQuestion,
+      message: "Question added successfully",
+      status: 201,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      status: 500,
+      message: "Something went wrong",
+    });
+  } finally {
+    prismaDb.$disconnect();
+  }
+}
---
+++ studyAi/src/app/api/question/updateQuestion.ts
@@ -0,0 +1,32 @@
+import { connectToDb, prismaDb } from "@/app/util/prisma/helpers";
+import { NextResponse } from "next/server";
+export async function updateQuestion(req: Request) {
+  try {
+    const bodyPromise = req.json();
+    const [body, _] = await Promise.all([bodyPromise, connectToDb()]);
+    const { userId } = body;
+    //add verify token here
+    const userCred = prismaDb.userCredentials.delete({
+      where: {
+        userId,
+      },
+    });
+    const user = prismaDb.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+    await Promise.all([userCred, user]);
+    return NextResponse.json({
+      message: "User deleted successfully",
+      status: 201,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      status: 500,
+      message: "Something went wrong",
+    });
+  } finally {
+    prismaDb.$disconnect();
+  }
+}
