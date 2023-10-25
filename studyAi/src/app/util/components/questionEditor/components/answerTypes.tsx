96696a19d3d930b99fa81b50a657ea8996c60981
Connor_Crump <ccrump@uoregon.edu>
2023-10-25 11:07:50 -0700

    setup answer types for question editor

---
+++ studyAi/src/app/util/components/questionEditor/components/answerTypes.tsx
@@ -0,0 +1,24 @@
+export const MultipleChoice = () => {
+    return (
+        <>
+            Multiple Choice
+        </>
+    )
+}
+
+export const SelectAll = () => {
+    return (
+        <>
+            Select All
+        </>
+    )
+}
+
+
+export const ShortAnswer = () => {
+    return (
+        <>
+            Short Answer
+        </>
+    )
+}
