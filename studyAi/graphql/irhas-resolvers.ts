cc37c305bb457bfb9e6492c82321280bfdf604f7
Irha Ali <irhaali_97@hotmail.ca>
2023-10-26 13:20:57 -0400

    Custom resolver in progress

--- studyAi/graphql/irhas-resolvers.ts
+++ studyAi/graphql/irhas-resolvers.ts
@@ -1,14 +1,13 @@
 /*
 Custom Resolvers Needed:
-- Create Question Routes (Irha)
-- Create User Info Routes (Irha)
+- Reads, Adds, deletes, and updates a user (Irha)
 */
 import { resolvers } from "../prisma/generated"
 
 @Resolver()
-export class GenerateAiQuestion {
-  @Query(returns => Question, { nullable: true })
-  async aiQuestion(@Ctx() { prisma }: Context): Promise<Question | null> {
+export class CustomUserResolver {
+  @Query(returns => User, { nullable: true })
+  async bestUser(@Ctx() { prisma }: Context): Promise<User | null> {
     return await prisma.user.findUnique({
       where: { email: "bob@prisma.io" },
     });
