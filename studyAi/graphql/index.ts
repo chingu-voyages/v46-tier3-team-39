7866b70a88e23bf7198a39d073cb456eb469ee05
Irha Ali <irhaali_97@hotmail.ca>
2023-10-26 13:09:54 -0400

    Update custom resolvers under graphql folder

--- studyAi/graphql/index.ts
+++ studyAi/graphql/index.ts
@@ -1,9 +1,7 @@
 /*
 Custom Resolvers Needed:
-- Generate a single question, using another question as a template + category + tag context (use OpenAI API) (Irha)
-- Adds, deletes, and updates a question's data into db ! (Fung)
-- Adds, deletes, and updates a set questions to a quiz (Fung)
-- Adds, deletes, and updates a quiz (Fung)
-- Create Question Routes (Irha)
-- Create User Info Routes (Irha)
+- Reads, Adds, deletes, and updates a question's data into db ! (Fung)
+- Reads, Adds, deletes, and updates a set questions to a quiz (Fung)
+- Reads, Adds, deletes, and updates a quiz (Fung)
+- Reads, Adds, deletes, and updates a user (Irha)
 */
