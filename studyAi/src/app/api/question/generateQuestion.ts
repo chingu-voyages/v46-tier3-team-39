d163a2db0dfa67dbc316358b56152eaaea6dfb48
Irha Ali <irhaali_97@hotmail.ca>
2023-10-30 15:36:16 -0400

    Complete generate question route and clean seedData file.

--- studyAi/src/app/api/question/generateQuestion.ts
+++ studyAi/src/app/api/question/generateQuestion.ts
@@ -13,15 +13,15 @@ const questionSchema = z.object({
 export async function generateQuestion(req: Request) {
   try {
     const bodyPromise = req.json();    
-    const [body] = await Promise.all([bodyPromise]);
+    const body = await bodyPromise;
     const { type, tags, question, answer } = questionSchema.parse(body);
     const questionType = (type === "mcq") ?  "multiple choice with four different potential answers" : ("checkbox") ? "with four different potential answers some correct and some incorrect" : "short answer";
     
-    const prompt = `Ask me a question, ${questionType}, similar to this question: ${question} and from the following subjects: ${tags}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {"Question": "", "Correct": "", "Incorrect": ["", "", ""]}`;
+    const prompt = `Ask me a question, ${questionType}, similar to this question: ${question} and from the following subjects: ${tags}. Indicate which is the correct response, and Return your response in a JSON object, with the following format: {"question": "", "correct": ["",...], "incorrect": ["",...]}`;
     const model = "gpt-3.5-turbo";
 
-    const newQuestion = await Promise.all([generatePrompts(model, prompt)]);
-    
+    const newQuestion = JSON.parse(await generatePrompts(model, prompt) || "");
+
     return NextResponse.json({
       newQuestion,
       message: `Question Generated Successfully`,
