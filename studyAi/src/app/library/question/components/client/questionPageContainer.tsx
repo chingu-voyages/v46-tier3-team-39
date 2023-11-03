5d3ac9c5bb51424dd303bb41dc4a9fa4b8df571c
Arky Asmal <arkyasmal@gmail.com>
2023-11-03 17:07:25 -0400

    added inital time modal component logic

--- studyAi/src/app/library/question/components/client/questionPageContainer.tsx
+++ studyAi/src/app/library/question/components/client/questionPageContainer.tsx
@@ -1,77 +1,11 @@
 "use client";
-import { useState } from "react";
 import { NavigationBtns, PaginationOptions } from "./navigationBtns";
 import { QuestionWrapper } from "./questionWrapper";
-import StopWatch from "@/app/util/components/time/stopwatch";
-import Timer from "@/app/util/components/time/timer";
-import { Button, Modal, Typography } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { TimeOptions } from "../../../../../../prisma/generated/type-graphql";
-//we can manage time on the frontend
-//because time measurements are only
-//for the user's benefit
-//if we need to ensure compliance to time
-//we must manage it using a websocket connection
-type TimeProps = TimeOptions & {
-  initialTime: number;
-};
-const TimeComponent = ({ props }: { props?: TimeProps }) => {
-  const { timeType, initialTime, totalTimeGiven } = props || {
-    initialTime: 0,
-  };
-  const [currType, setCurrType] = useState(timeType);
-  const [currInitType, setCurrInitType] = useState(initialTime);
-  const [currTotalTimeGiven, setCurrTotalTimeGiven] = useState(totalTimeGiven);
-  const [modalOpen, setModalOpen] = useState(true);
-  switch (currType) {
-    case "stopwatch":
-      return <StopWatch initialTimeUsed={initialTime} />;
-    case "timer":
-      return (
-        <Timer
-          initialTimeLeft={initialTime}
-          totalTimeGiven={currTotalTimeGiven}
-        />
-      );
-    //create timer component
-    default:
-      return (
-        <>
-          {!modalOpen && (
-            <Button
-              type="button"
-              onClick={() => setModalOpen(true)}
-              className="h-full"
-              sx={{ textTransform: "unset", minHeight: "unset" }}
-              aria-label="open-modal-to-attach-stopwatch-or-timer"
-            >
-              <FontAwesomeIcon icon={faPlus} />
-              <span className="ml-1">Add Time</span>
-            </Button>
-          )}
-          <Modal
-            open={modalOpen}
-            onClose={() => setModalOpen(false)}
-            aria-labelledby="track-your-time"
-            aria-describedby="attach-stopwatch-or-timer"
-            className="flex justify-center items-center"
-          >
-            <div className="w-4/6 min-h-[80%] flex justify-center bg-White p-[3%] overflow-y">
-              <Typography variant="h6" component="h2" className="text-Black">
-                Track Your Time
-              </Typography>
-
-            </div>
-          </Modal>
-        </>
-      );
-  }
-};
+import { TimeComponent } from "./timeComponent";
 const QuestionFormWrapper = ({ children }: { children: React.ReactNode }) => {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    //grab uncontrolled inputs here
+    //grab uncontrolled inputs here form
     const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData.entries());
     const { email } = data;
