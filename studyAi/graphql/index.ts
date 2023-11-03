696f00667becee8cb9d3469d9b64b1e3f34585d5
Arky Asmal <arkyasmal@gmail.com>
2023-11-03 18:05:44 -0400

    fixed import issues

--- studyAi/graphql/index.ts
+++ studyAi/graphql/index.ts
@@ -1,10 +1,19 @@
-import { resolvers } from "../prisma/generated/type-graphql";
-import { Resolver, Arg, Args, Ctx, Query, Mutation, Info} from "type-graphql";
+import {
+  FindUniqueQuestionArgs,
+  resolvers,
+} from "../prisma/generated/type-graphql";
+import { Resolver, Arg, Args, Ctx, Query, Mutation, Info } from "type-graphql";
 import type { GraphQLResolveInfo } from "graphql";
-import { FindUniqueQuestionArgs } from "/Users/tiff/Desktop/react/v46-tier3-team-39/studyAi/prisma/generated/type-graphql/resolvers/crud/Question/args/FindUniqueQuestionArgs";
-import { Question } from "/Users/tiff/Desktop/react/v46-tier3-team-39/studyAi/prisma/generated/type-graphql/models/Question";
-import { transformInfoIntoPrismaArgs, getPrismaFromContext, transformCountFieldIntoSelectRelationsCount } from "/Users/tiff/Desktop/react/v46-tier3-team-39/studyAi/prisma/generated/type-graphql/helpers";
+import { Question } from "../prisma/generated/type-graphql";
 import { NonEmptyArray } from "type-graphql";
+// import { FindUniqueQuestionArgs } from "/Users/tiff/Desktop/react/v46-tier3-team-39/studyAi/prisma/generated/type-graphql/resolvers/crud/Question/args/FindUniqueQuestionArgs";
+// import { Question } from "/Users/tiff/Desktop/react/v46-tier3-team-39/studyAi/prisma/generated/type-graphql/models/Question";
+import {
+  transformInfoIntoPrismaArgs,
+  getPrismaFromContext,
+  transformCountFieldIntoSelectRelationsCount,
+} from "../prisma/generated/type-graphql/helpers";
+// import { NonEmptyArray } from "type-graphql";
 
 // Custom Resolvers : https://prisma.typegraphql.com/docs/advanced/custom-operations/
 // How to test the resolvers: https://docs.google.com/presentation/d/16v_wdvdu2-88dA7O1jAxj_BqbT7SMPn6Ig_7VL81o64/edit#slide=id.p3
@@ -30,81 +39,90 @@ Custom Resolvers Needed:
 // 1. Verify the user can edit the data (for verification. session userId = question/quiz/user userId)
 // 2. Use the auto-generated resolver
 
-@Resolver(_of => Question)
+@Resolver((_of) => Question)
 export class FindUniqueQuestionResolver {
-  // @Query(_returns => Question, {
-  //   nullable: true
-  // })
-  // async question(@Ctx() ctx: any, @Info() info: GraphQLResolveInfo,
-  // @Args() args: FindUniqueQuestionArgs): Promise<Question | null> {
-  //   const { _count } = transformInfoIntoPrismaArgs(info);
-  //   const question = getPrismaFromContext(ctx).question.findUnique({
-  //     ...args,
-  //     ...(_count && transformCountFieldIntoSelectRelationsCount(_count)),
-  //   });
-  //   if (!ctx.session || ctx.session.userId !== question.creatorId) {
-  //     throw new Error("You are not authorized to perform this action");
-  //   }
-  //   return question;
-  // }
-
-  @Query(_returns => Question, {
-    nullable: true
+  @Query((_returns) => Question, {
+    nullable: true,
   })
-  async readQuestion(@Arg('id') id: string, @Ctx() {req, res, prisma, session}: any)
-    : Promise<Question | null> {
-      const question = await prisma.question.findUnique({
-        where: { id: id }});
-      if (!session || session.userId !== question.creatorId) {
-        throw new Error("You are not authorized to perform this action");
-      }
-      prisma.$disconnect();
-      return question;
+  async question(
+    @Ctx() ctx: any,
+    @Info() info: GraphQLResolveInfo,
+    @Args() args: FindUniqueQuestionArgs
+  ): Promise<Question | null> {
+    const { _count } = transformInfoIntoPrismaArgs(info);
+    const question = getPrismaFromContext(ctx).question.findUnique({
+      ...args,
+      ...(_count && transformCountFieldIntoSelectRelationsCount(_count)),
+    });
+    if (!ctx.session || ctx.session.userId !== question.creatorId) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    return question;
   }
 
-  @Mutation(_returns => Question, {
-    nullable: true
+  @Query((_returns) => Question, {
+    nullable: true,
   })
-  async addQuestion(@Arg('questionType') questionType: string,
-    @Arg('tags') tags: string[], @Arg('questionTitle') questionTitle: string,
-    @Arg('questionDesc') questionDesc: string,
-    @Arg('correctAnswer') correctAnswer: string[],
-    @Arg('incorrectAnswer') incorrectAnswer: string[],
-    @Ctx() {req, res, prisma, session}: any)
-    : Promise<Question | null> {
-      const questionData = await prisma.questionData.create({
-        data: {
-          questionTitle: questionTitle,
-          questionDesc: questionDesc,
-        }
-      })
-      const answer = await prisma.answer.create({
-        data: {
-          correctAnswer: correctAnswer,
-          incorrectAnswer: incorrectAnswer,
-        }
-      })
-      const creator = await prisma.user.findUnique({
-        where: { id: session.userId }
-      })
-      const likeCounter = await prisma.likeCounter.create({
-        likes: 0,
-        dislikes: 0
-      })
-      const question = await prisma.question.create({
-        data: {
-          creatorId: creator.id,
-          questionType: questionType,
-          tags: tags,
-          question: questionData,
-          answer: answer,
-          likeCounter: likeCounter
-        }
-      });
-      prisma.$disconnect();
-      return question;
+  async readQuestion(
+    @Arg("id") id: string,
+    @Ctx() { req, res, prisma, session }: any
+  ): Promise<Question | null> {
+    const question = await prisma.question.findUnique({
+      where: { id: id },
+    });
+    if (!session || session.userId !== question.creatorId) {
+      throw new Error("You are not authorized to perform this action");
     }
+    prisma.$disconnect();
+    return question;
+  }
+
+  @Mutation((_returns) => Question, {
+    nullable: true,
+  })
+  async addQuestion(
+    @Arg("questionType") questionType: string,
+    @Arg("tags") tags: string[],
+    @Arg("questionTitle") questionTitle: string,
+    @Arg("questionDesc") questionDesc: string,
+    @Arg("correctAnswer") correctAnswer: string[],
+    @Arg("incorrectAnswer") incorrectAnswer: string[],
+    @Ctx() { req, res, prisma, session }: any
+  ): Promise<Question | null> {
+    const questionData = await prisma.questionData.create({
+      data: {
+        questionTitle: questionTitle,
+        questionDesc: questionDesc,
+      },
+    });
+    const answer = await prisma.answer.create({
+      data: {
+        correctAnswer: correctAnswer,
+        incorrectAnswer: incorrectAnswer,
+      },
+    });
+    const creator = await prisma.user.findUnique({
+      where: { id: session.userId },
+    });
+    const likeCounter = await prisma.likeCounter.create({
+      likes: 0,
+      dislikes: 0,
+    });
+    const question = await prisma.question.create({
+      data: {
+        creatorId: creator.id,
+        questionType: questionType,
+        tags: tags,
+        question: questionData,
+        answer: answer,
+        likeCounter: likeCounter,
+      },
+    });
+    prisma.$disconnect();
+    return question;
+  }
 }
 
-
-export const allResolvers: NonEmptyArray<Function>= [FindUniqueQuestionResolver]
+export const allResolvers: NonEmptyArray<Function> = [
+  FindUniqueQuestionResolver,
+];
