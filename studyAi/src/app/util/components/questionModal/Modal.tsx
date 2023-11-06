6a17f05be4689c5339d70bb78464a15870b32b8d
Connor_Crump <ccrump@uoregon.edu>
2023-11-06 11:22:58 -0800

    fix styles and pass logic to cancel and exit buttons

--- studyAi/src/app/util/components/questionModal/Modal.tsx
+++ studyAi/src/app/util/components/questionModal/Modal.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Modal from '@mui/material/Modal'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import LeftContent from './components/leftContent/leftContent'
@@ -8,28 +7,22 @@ import AnswerEditor from './components/answerEditor/answerEditor'
 import Controls from './components/controls'
 import styles from './ModalStyles'
 import { useQuestions } from '@/app/stores/questionStore'
-import { useState } from 'react'
 
-const QuestionModal = ({questionId}: {questionId?: string}) => {
+const QuestionEditForm = ({questionId, setIsOpen}: {questionId?: string, setIsOpen: React.Dispatch<React.SetStateAction<boolean>>}) => {
 
     const questionData = questionId ? useQuestions()[0].data[questionId]: undefined;
-    const [isOpen, setIsOpen] = useState(true)
     
     return (
-        <Modal
-            open={isOpen}
-        >
-            <div className={styles.modal}>
-                <FontAwesomeIcon icon={faXmark} className={styles.closeIcon} onClick={() => setIsOpen(false)} />
-                <h1 className={styles.h1}>Question Editor</h1>
-                <div className={styles.contentLayout}>
-                    <LeftContent questionData={questionData}/>
-                    <AnswerEditor />
-                </div>
-                <Controls />
+        <div className={styles.modal}>
+            <FontAwesomeIcon icon={faXmark} className={styles.closeIcon} onClick={() => setIsOpen(false)}/>
+            <h1 className={styles.h1}>Question Editor</h1>
+            <Controls setIsOpen={setIsOpen} />
+            <div className={styles.contentLayout}>
+                <LeftContent questionData={questionData}/>
+                <AnswerEditor />
             </div>
-        </Modal>
+        </div>
     )
 }
 
-export default QuestionModal;
+export default QuestionEditForm;
