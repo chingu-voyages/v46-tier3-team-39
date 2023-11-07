6082ba0cdb77a6e3ac80625ed57967049fde13e5
Irha Ali <114361467+IrhaAli@users.noreply.github.com>
2023-11-07 01:01:48 -0500

    Update graphql.ts

--- studyAi/src/app/api/graphql/graphql.ts
+++ studyAi/src/app/api/graphql/graphql.ts
@@ -2,98 +2,35 @@ import "reflect-metadata";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "./lib/startServerAndCreateNextHandler";
 import { prismaDb } from "@/app/util/prisma/connection";
-import { getServerSession } from "next-auth";
-import { options } from "../auth/[...nextauth]/options";
 import { Session } from "next-auth";
 import { NextApiRequest, NextApiResponse } from "next";
-import { GraphQLError, parse } from "graphql";
 import { createSchema } from "../../../../graphql/createSchema";
-
+import validateAuthRequirementInQuery, {
+  getSession,
+} from "./validateAuthRequirementInQuery";
 const server = new ApolloServer({
   schema: await createSchema(),
 });
 
-const getSession = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    res.getHeader = (name: string) => res.headers?.get(name);
-    res.setHeader = (name: string, value: string) =>
-    res.headers?.set(name, value);
-    return await getServerSession(req, res, options);
-  } catch (e) {
-     return null;
-  }
-}
-
-const getParsedQuery = (queryString: string) => {
-  (!queryString.includes("where")) ? canUserModify(null, null, "Improper Query") : null;
-  try {
-    return parse(queryString);
-  } catch (e) {
-    canUserModify(null, null, "Improper Query");
-  }
-}
-
- const canUserModify = (session: Session | null, actualId: string | null, message: string) => {
-  if (!session || session?.user?.id !== actualId)
-    throw new GraphQLError(message, {
-      extensions: {
-        code: "UNAUTHENTICATED",
-        http: { status: 401 },
-      },
-    });
-};
-
-const validateVariables = (resolverRequested : string, variables: { public: boolean | null, actualId: string | null, take: number | null}, accessibleModels: string[]) => {
-  if ((accessibleModels.includes(resolverRequested) && variables.public === null) || !variables.actualId || !variables.take) {
-    canUserModify(null, null, "Improper Query");
-  }
-}
-
 const main = startServerAndCreateNextHandler(server, {
-  context: async (req: any, res: any) => {
+  context: async (
+    req: NextApiRequest & { graphQLBody: any },
+    res: NextApiResponse
+  ) => {
     const body = await req.graphQLBody;
-    // Get all variables (actualId, take, public (only for quiz and question))
-    const variables: { actualId: string | null, public: boolean | null, take: number | null} = {
-      actualId: body.variables.id || null,
-      public: !body.variables.private || null,
-      take: body.variables.take || null
-    };
-    const parsedQuery = getParsedQuery(body.query);
-    const resolverRequested = parsedQuery?.definitions[0].selectionSet.selections[0].name.value;
-    const accessibleModels = ["question", "quiz"];
-    const isQuery = parsedQuery?.definitions[0].operation.toLowerCase() === "query";
-
     const session: Session | null = await getSession(req, res);
-    req = req as NextApiRequest;
-    res = res as NextApiResponse;
-
-
-    // Validate if there is an id, take and [public (only for quiz and question)]
-    validateVariables(resolverRequested, variables, accessibleModels);
-
-    if (isQuery) {
-      switch (resolverRequested) {
-        case "questions": case "quizzes":
-          if (!variables.public) {
-            canUserModify(session, body.variables.actualId, "User is not authorized/authenticated");
-          }
-          break;
-        default:
-          canUserModify(session, variables.actualId, "User is not authorized/authenticated");
-      }
-    } else {
-      canUserModify(session, variables.actualId, "User is not authorized/authenticated");
-    }
-
+    //throw error if query requires authentication and user is not authenticated
+    //or does not have the proper read/write access rights
+    // validateAuthRequirementInQuery({ session, body });
     const contextData = {
       req,
       res,
       prisma: prismaDb,
-      session
+      session,
     };
 
     return contextData;
-  }
+  },
 });
 
 export default main;
