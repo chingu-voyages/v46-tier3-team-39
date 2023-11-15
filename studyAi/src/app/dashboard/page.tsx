9181cb62251a8540fdbe96127e3e7c3946820b08
aasmal97 <74555081+aasmal97@users.noreply.github.com>
2023-11-14 20:19:55 -0500

    fixed cursor error in questions query

--- studyAi/src/app/dashboard/page.tsx
+++ studyAi/src/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import { protectRouteSSR } from "../api/utils/sessionFuncs";
 import NavigationWrapper from "../util/components/navigation/navigationWrapper";
 import GreetingBannerContainer from "./server/greetingBannerContainer";
 import Profile from "./server/profile";
-import Blah from "./blah";
 
 export default async function DashboardPage() {
   const sessionData = await protectRouteSSR("/auth/login");
