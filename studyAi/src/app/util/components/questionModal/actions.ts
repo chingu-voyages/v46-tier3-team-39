5812857719ea4a7e56a7abff9e7dc7c4d8078e5c
Arky Asmal <arkyasmal@gmail.com>
2023-12-04 02:23:08 -0500

    fixed typename invalid value error

--- studyAi/src/app/util/components/questionModal/actions.ts
+++ studyAi/src/app/util/components/questionModal/actions.ts
@@ -14,6 +14,7 @@ import {
   UpdateSingleQuestionMutation,
 } from "../../../../../graphql/generated/graphql";
 import { GetQuestionCreator } from "@/gql/queries/questionQueries";
+import removeTypename from "../../parsers/removeTypename";
 export const uploadQuestionToDb = async ({
   questionData,
   questionId,
@@ -73,52 +74,56 @@ export const uploadQuestionToDb = async ({
     },
     private: !!questionData?.private,
   };
+  const updateQuestionVariables = questionId
+    ? {
+        id: questionId,
+        questionInfo: questionData.questionInfo
+          ? {
+              update: {
+                title: questionData.questionInfo.title
+                  ? { set: questionData.questionInfo.title }
+                  : undefined,
+                description: questionData.questionInfo.description
+                  ? { set: questionData.questionInfo.description }
+                  : undefined,
+                options: questionData.questionInfo.options,
+              },
+            }
+          : undefined,
+        answer:
+          questionData.answer && questionData.answer.correctAnswer
+            ? {
+                update: {
+                  correctAnswer: questionData.answer.correctAnswer,
+                },
+              }
+            : undefined,
+        tags: questionData.tags
+          ? {
+              set: questionData.tags,
+            }
+          : undefined,
+        questionType: questionData.questionType
+          ? { set: questionData.questionType }
+          : undefined,
+        private:
+          typeof questionData.private === "boolean"
+            ? { set: questionData.private }
+            : undefined,
+      }
+    : null;
+
   const [questionResult, _] = await Promise.all([
     //update question doc
     //if question id is present, update the question doc
-    questionId
+    questionId && updateQuestionVariables
       ? client.mutate({
           mutation: UpdateQuestionMutation,
-          variables: {
-            id: questionId,
-            questionInfo: questionData.questionInfo
-              ? {
-                  update: {
-                    title: questionData.questionInfo.title
-                      ? { set: questionData.questionInfo.title }
-                      : undefined,
-                    description: questionData.questionInfo.description
-                      ? { set: questionData.questionInfo.description }
-                      : undefined,
-                    options: questionData.questionInfo.options,
-                  },
-                }
-              : undefined,
-            answer:
-              questionData.answer && questionData.answer.correctAnswer
-                ? {
-                    update: {
-                      correctAnswer: questionData.answer.correctAnswer,
-                    },
-                  }
-                : undefined,
-            tags: questionData.tags
-              ? {
-                  set: questionData.tags,
-                }
-              : undefined,
-            questionType: questionData.questionType
-              ? { set: questionData.questionType }
-              : undefined,
-            private:
-              typeof questionData.private === "boolean"
-                ? { set: questionData.private }
-                : undefined,
-          },
+          variables: removeTypename(updateQuestionVariables),
         })
       : client.mutate({
           mutation: AddQuestionMutation,
-          variables: variables,
+          variables: removeTypename(variables),
         }),
     //increment user data count
     questionId
